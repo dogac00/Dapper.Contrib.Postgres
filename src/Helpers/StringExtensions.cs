1c2350613898922c4de8cfe112aac6c03f592b7c
Dogac <ndakyildiz@gmail.com>
2020-12-02 22:47:39 +0300

    Add RemoveWhitespace to StringExtensions.

--- src/Helpers/StringExtensions.cs
+++ src/Helpers/StringExtensions.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 namespace Dapper.Contrib.Postgres.Helpers
 {
     public static class StringExtensions
@@ -5,6 +7,13 @@ namespace Dapper.Contrib.Postgres.Helpers
         public static string AddQuotes(this string value)
         {
             return $"\"{value}\"";
+        }
+        
+        public static string RemoveWhitespace(this string value)
+        {
+            return new string(value
+                .Where(c => !char.IsWhiteSpace(c))
+                .ToArray());
         }
     }
 }
