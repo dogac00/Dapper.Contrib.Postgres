8097f796a89fc712d91b904f0ef7528b77981825
Dogac <ndakyildiz@gmail.com>
2020-12-03 22:56:31 +0300

    Add Generic Insert Method.

---
+++ Dapper.Contrib.Postgres.IntegrationTests/Tests/DatabaseHelper.cs
@@ -0,0 +1,24 @@
+using System.Data;
+using System.Threading.Tasks;
+using Dapper.Contrib.Postgres.IntegrationTests.TestTypes;
+using Npgsql;
+
+namespace Dapper.Contrib.Postgres.IntegrationTests.Tests
+{
+    public class DatabaseHelper
+    {
+        public static async Task InsertGeneric<T>(IDbConnection connection, T entity)
+        {
+            if (connection is NpgsqlConnection)
+            {
+                await connection.InsertAsync(entity);
+            }
+            else
+            {
+                var sql = Extensions.GetInsertSql<Employee>();
+
+                await connection.ExecuteAsync(sql, entity);
+            }
+        }
+    }
+}
