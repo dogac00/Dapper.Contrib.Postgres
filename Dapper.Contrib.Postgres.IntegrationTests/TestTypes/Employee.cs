2c5598f2214fbb58c1d546eef6eb46c0787e0221
Dogac <ndakyildiz@gmail.com>
2020-12-03 22:54:31 +0300

    Rename Employee1 to Employee.

--- Dapper.Contrib.Postgres.IntegrationTests/TestTypes/Employee.cs
+++ Dapper.Contrib.Postgres.IntegrationTests/TestTypes/Employee.cs
@@ -3,7 +3,7 @@ using Dapper.Contrib.Postgres.Attributes;
 namespace Dapper.Contrib.Postgres.IntegrationTests.TestTypes
 {
     [UseQuotedIdentifiers]
-    public class Employee1
+    public class Employee
     {
         public long Id { get; set; }
         public string Name { get; set; }
