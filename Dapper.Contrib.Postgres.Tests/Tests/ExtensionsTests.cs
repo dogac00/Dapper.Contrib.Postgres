d52d2b2fccdb0c84b614bd1016b6bfbec75e4e9e
Dogac <ndakyildiz@gmail.com>
2020-12-04 12:31:50 +0300

    Update ExtensionsTests, Update Namespace.

--- Dapper.Contrib.Postgres.Tests/Tests/ExtensionsTests.cs
+++ Dapper.Contrib.Postgres.Tests/Tests/ExtensionsTests.cs
@@ -1,5 +1,5 @@
 using System.Collections.Generic;
-using Dapper.Contrib.Postgres.Helpers;
+using Dapper.Contrib.Postgres.Tests.Helpers;
 using Dapper.Contrib.Postgres.Tests.TestTypes;
 using FluentAssertions;
 using NUnit.Framework;
