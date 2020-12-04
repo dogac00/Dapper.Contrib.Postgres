42487d126a348234118e948f389320b43b4699cf
Dogac <ndakyildiz@gmail.com>
2020-12-04 12:25:56 +0300

    Remove Unnecessary Method From StringExtensions in Project.

--- src/Helpers/StringExtensions.cs
+++ src/Helpers/StringExtensions.cs
@@ -1,19 +1,10 @@
-using System.Linq;
-
 namespace Dapper.Contrib.Postgres.Helpers
 {
-    public static class StringExtensions
+    internal static class StringExtensions
     {
         public static string AddQuotes(this string value)
         {
             return $"\"{value}\"";
-        }
-        
-        public static string RemoveWhitespace(this string value)
-        {
-            return new string(value
-                .Where(c => !char.IsWhiteSpace(c))
-                .ToArray());
         }
     }
 }
