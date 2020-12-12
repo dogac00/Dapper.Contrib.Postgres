093848e145d68faab72fe6e3cdcb7276b228a3ad
Dogac <ndakyildiz@gmail.com>
2020-12-12 22:34:06 +0300

    Update ConnectionExtensions, Add Utility Methods.

--- Dapper.Contrib.Postgres.IntegrationTests/Extensions/ConnectionExtensions.cs
+++ Dapper.Contrib.Postgres.IntegrationTests/Extensions/ConnectionExtensions.cs
@@ -1,9 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Dapper.Contrib.Postgres.Attributes;
-using Dapper.Contrib.Postgres.Helpers;
 
 namespace Dapper.Contrib.Postgres.IntegrationTests.Extensions
 {
@@ -21,19 +22,29 @@ namespace Dapper.Contrib.Postgres.IntegrationTests.Extensions
         {
             var typeName = typeof(T).Name;
             var pluralTypeName = typeName + "s";
-            var tableAttribute = typeof(T).GetAttribute<TableAttribute>();
+            var tableAttribute = GetAttribute<TableAttribute>(typeof(T));
 
             if (tableAttribute != null)
             {
                 return tableAttribute.Name;
             }
 
-            if (typeof(T).HasAttribute<UseQuotedIdentifiersAttribute>())
+            if (HasAttribute<UseQuotedIdentifiersAttribute>(typeof(T)))
             {
                 return '"' + pluralTypeName + '"';
             }
 
             return pluralTypeName;
+        }
+
+        private static bool HasAttribute<T>(Type type) where T : Attribute
+        {
+            return type.GetCustomAttribute(typeof(T)) != null;
+        }
+
+        private static T GetAttribute<T>(Type type) where T : Attribute
+        {
+            return type.GetCustomAttribute(typeof(T)) as T;
         }
     }
 }
